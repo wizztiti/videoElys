9d7a9c32358ed401f4afa82456b5bf63da7b304c
ThierryDC <thierry@del-castilo.eu>
2018-06-26 17:21:07 +0200

    creation table entite

---
+++ database/migrations/2018_06_26_144935_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->time('duration');
+            $table->string('teaser_file')->unique();
+            $table->string('video_file')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
