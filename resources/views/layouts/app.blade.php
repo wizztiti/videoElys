51e04c5ae361e45a6d7b74fe91debc3ba408f08e
del Castillo <Thierry>
2018-08-28 16:33:18 +0200

    FIX :  Systeme des tags (suppression de TailWindCSS qui entre en conflit avec Bootstrap)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack('javascript-libs');
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
@@ -77,10 +81,6 @@
             @yield('content')
         </main>
     </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @stack('javascript-libs');
 
 </body>
 </html>
