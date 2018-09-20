c2fe3d39418c48913eb1fb4c91a8670bf801228d
Nicolas DEL CASTILLO <ndelcast.dev@gmail.com>
2018-09-20 17:37:35 +0200

    - FIX footer take all width of the navigator window - FIX webpack config - Unactive Debugbar

--- resources/views/public/post-index.blade.php
+++ resources/views/public/post-index.blade.php
@@ -27,6 +27,25 @@
                 </div>
             @endforeach
 
+            @foreach($posts as $post)
+                <div class="col-xs col-sm-6 col-md-4">
+                    <div class="post">
+                        <a href="/post/{{ $post->category->slug }}/{{ $post->slug }}"><img src="/img/teaser-post.jpg" class="img-fluid"></a>
+                        <div class="resume">
+                            <h4><a href="/post/{{ $post->category->slug }}/{{ $post->slug }}">{!! $post->title !!}</a></h4>
+                            <div class="text">
+                                {{ substr($post->text, 0, 120) }}
+                            </div>
+                            <div class="links">
+                                <div class="love"><img src="" alt="">609</div>
+                                <div class="comments"><img src="" alt="">120</div>
+                                <span class="share">Share</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
         </div>
     </div>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,7 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.postCss('resources/assets/css/app.css', 'public/css');
+// mix.postCss('resources/assets/css/app.css', 'public/css');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
